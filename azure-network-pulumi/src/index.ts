4c975d0ab675d82209a22588e4cae9b2f18235c6
Jose Miguel <jburitica@arkusnexus.com>
2025-12-19 18:06:16 -0500

    Add subnet3 definition and update resource group name initialization

--- azure-network-pulumi/src/index.ts
+++ azure-network-pulumi/src/index.ts
@@ -5,7 +5,7 @@ import { NSG } from "./network/nsg";
 import { azureConfig } from "./config";
 
 // Create Resource Group
-const resourceGroup = new azure.resources.ResourceGroup("rg", {
+const resourceGroup = new azure.resources.ResourceGroup("", {
     resourceGroupName: azureConfig.resourceGroupName,
     location: azureConfig.location
 });
@@ -42,10 +42,19 @@ const subnet2 = new Subnet(
     nsg.nsg.id
 );
 
+const subnet3 = new Subnet(
+    azureConfig.subnet3Name,
+    resourceGroup.name,
+    vnet.vnet.name,
+    azureConfig.subnet3AddressPrefix,
+    nsg.nsg.id
+);
+
 // Export the IDs of the created resources
 export const resourceGroupId = resourceGroup.id;
 export const vnetId = vnet.vnet.id;
 export const subnetId = subnet.subnet.id;
 export const subnet2Id = subnet2.subnet.id;
+export const subnet3Id = subnet3.subnet.id;
 export const nsgId = nsg.nsg.id;
 
